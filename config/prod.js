db33d30e369063a75bc607c098a969a129bb7c5d
hmq586 <39184763+hmq586@users.noreply.github.com>
2018-09-04 17:22:45 +0800

    Update prod.js

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,7 @@
 const commonConfig = require('./common');
 const prodConfig = Object.assign({}, commonConfig, {
   secret: {
+    token: 'wechat123456',
     appid: 'wx27da4d9c25135f8b',
     secret: 'b8a6ac1939347ca5fddc4ebeca204710'
   }
