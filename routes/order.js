32e3b189b0f28c08a109d0d40576328f557962c0
hmq586 <clark.huang@sap.com>
2018-10-23 13:25:02 +0800

    sd

--- routes/order.js
+++ routes/order.js
@@ -80,7 +80,7 @@ router.post('/create', function (req, res, next) {
       //     sendkFMessage(openId, `Sales order ${id} had been created successfully!` ,res);
     });
    // const salesOrderId = salesOrder();
-   const salesOrderId
+   const salesOrderId;
    salesOrder.then(function(id){
     salesOrderId = id;
    })
