3fe4265628efb9ab510548e518701ca1090b0094
hmq586 <clark.huang@sap.com>
2019-04-15 10:35:32 +0800

    st

--- routes/status.js
+++ routes/status.js
@@ -67,7 +67,10 @@ router.post('/push', function (req, res, next) {
     const lfimg = req.body.lfimg;
     const material = req.body.material;
     sendkFMessage(openId, `Sales Order <a href="${getSigninUrl_d(Id)}">${Id}</a> is in process. Outbound delivery ${sOD} send ${lfimg}L ${material}` ,res);
-  }else{
+  }else if(sType === "B"){
+    sendkFMessage(openId, `Sales Order <a href="${getSigninUrl(Id)}">${Id}</a> has been approved.` ,res);
+  }
+  else{
     const status = req.body.status;
     const bstatus = req.body.bstatus;
     if (bstatus === "Unknow Status") {
