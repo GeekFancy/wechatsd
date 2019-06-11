c801de807e50b1ed5900c3448979b736ebc6beec
GeekFancy <794951421@qq.com>
2019-06-11 17:14:48 +0800

    Update prod.js

--- config/prod.js
+++ config/prod.js
@@ -2,11 +2,11 @@ const commonConfig = require('./common');
 const prodConfig = Object.assign({}, commonConfig, {
   secret: {
     token: 'wechat123456',
-    appid: 'wx27da4d9c25135f8b',
-    secret: 'b8a6ac1939347ca5fddc4ebeca204710'
+    appid: 'wxb4d82dee891d83aa',
+    secret: 'aebc551dcf318d050b86da4332235ea7'
   },
-  server: 'https://wechatsd.herokuapp.com/wechat',
-  uiServer: 'https://wechatsd.herokuapp.com/sd_wechat_ui-master/',
+  server: 'https://wechat-intergration.herokuapp.com/wechat',
+  uiServer: 'https://wechat-intergration.herokuapp.com/sd_wechat_ui-master/',
   sdApi: 'https://ldai1cc2.wdf.sap.corp:50001/sap/opu/odata/sap/API_SALES_ORDER_WITHOUT_CHARGE_SRV/A_SalesOrderWithoutCharge',
   credential: '_SAPI072128:x[MeZU6e[7cdkRk%jse2'
 });
