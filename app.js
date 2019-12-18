b1f00e347b4dd4ca1193399c95fdc7f8c95fce50
GeekFancy <794951421@qq.com>
2019-12-18 13:19:57 +0800

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://wechat-intergration.herokuapp.com/sd_wechat_ui-master/");
+  res.header("Access-Control-Allow-Origin", "https://wechatforsd.herokuapp.com/sd_wechat_ui-master/");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
